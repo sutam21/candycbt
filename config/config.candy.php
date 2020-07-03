5e1fcba5c3f8187e580e1a7e408580e7ca389239
sutam21 <servergiveaway@gmail.com>
2020-07-03 16:08:38 +0700

    add config.candy.php

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'Candy CBT';
+
+define("VERSI", "2.8.0");
+define("REVISI", "3");
+define("APLIKASI", "Candy CBT");
+define("NAMA_DATABASE", "cbtcandy28");
+define("VERSI_DB", "2.8.1");
+define("BASEPATH", "cbtcandy");
